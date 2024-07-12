b992972a9ffb29dce4ba5415f01575a32824fbb4
Shivam <shivamgpt34@gmail.com>
2024-07-12 10:42:18 -0600

    feat: Fix typo in secureURL property name

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -100,7 +100,7 @@ const TransformationForm = ({
         width: image?.width,
         height: image?.height,
         config: transformationConfig,
-        secureURL: image?.secureUrl,
+        secureURL: image?.secureURL,
         transformationURL: transformationUrl,
         aspectRatio: values.aspectRatio,
         prompt: values.prompt,
